3a83fa1cc8e9fb0b592176f59bfaafdd1baf7f14
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2025-09-17 15:45:00 +0530

    java-1

---
+++ if elif/one.java
@@ -0,0 +1,15 @@
+class one
+{
+    public static void main(String args[])
+    {
+        int x=5;
+        int y=10;
+        int z=15;
+        if(x>y && x>z)
+            System.out.println(x);
+        else if(y>x && y>z)
+            System.out.println(y);
+        else
+            System.out.println(z);
+    }
+}
