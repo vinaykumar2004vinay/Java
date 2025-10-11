6c7f524766d14796115649bbcba69c7338425cd5
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2025-10-11 10:35:05 +0530

    Java

---
+++ Exception/one.java
@@ -0,0 +1,9 @@
+public class one 
+{
+    public static void main(String a[])
+    {
+        int i = 9;
+        int j = 18/i;
+        System.out.println(j);
+    }
+}
---
+++ Exception/three.java
@@ -0,0 +1,18 @@
+public class three
+{
+    public static void main(String a[])
+    {
+        int i = 0;
+        int j = 0;
+        try
+        {
+            j = 18/i;
+        }
+        catch(Exception e)
+        {
+            System.out.println("Some Thing Went Wrong....");
+        }
+        System.out.println(j);
+        System.out.println("Byeee....");
+    }
+}
